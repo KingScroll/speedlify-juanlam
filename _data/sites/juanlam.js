06c83b4ad523ac31f06b08c99ba63430f3da6342
KingScroll <69558037+KingScroll@users.noreply.github.com>
2022-11-16 11:21:08 -0500

    Update juanlam.js

--- _data/sites/juanlam.js
+++ _data/sites/juanlam.js
@@ -1,10 +1,14 @@
 module.exports = {
 	name: "Personal Site", // optional, falls back to object key
-	description: "My primary website",
+	description: "Juan Lam Primary Site",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://juanlam.com/"
+		"https://juanlam.com/",
+		"https://juanlam.com/about/",
+		"https://juanlam.com/stories/",
+		"https://juanlam.com/projects/",
+		"https://juanlam.com/blog/"
 	]
 };
