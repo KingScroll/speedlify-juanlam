d76e61fd5c04ceffadd8608b3b63567ae60d4bc2
KingScroll <69558037+KingScroll@users.noreply.github.com>
2022-11-16 11:06:45 -0500

    Update and rename sample.js to juanlam.js

---
+++ _data/sites/juanlam.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Personal Site", // optional, falls back to object key
+	description: "My primary website",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://juanlam.com/"
+	]
+};
